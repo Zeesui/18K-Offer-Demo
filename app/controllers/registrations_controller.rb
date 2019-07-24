bacdd47a5a43b782041eae1045261a69a47eb9de
zee <kooo9671354@gmail.com>
2019-07-24 16:58:05 +0800

    implement devise

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationsController < Devise::RegistrationsController
+  private
+
+  def sin_up_params
+    params.require(:user).permit(:user_name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:user_name, :email, :password, :password_confirmation, :current_password)
+  end
+end
